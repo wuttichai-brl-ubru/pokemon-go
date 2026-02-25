ee720d06fad3f8e08e7cd407ee4ec8a70d400c70
wuttichai-brl-ubru <std.67122420309@ubru.ac.th>
2026-02-25 11:34:24 +0700

    fix bug in core/routes.py

--- pokemon/core/routes.py
+++ pokemon/core/routes.py
@@ -6,8 +6,8 @@ core_bp = Blueprint('core', __name__, template_folder='templates')
 
 @core_bp.route('/')
 def index():
-  page = request.args.get('page')
-  pokemons = db.paginate(db.select(Pokemon), per_page=4, page=page)
+  page = request.args.get('page', type=int)
+  pokemons = db.paginate(sa.select(Pokemon), per_page=4, page=page)
   return render_template('core/index.html',
                           title='Home Page',
                           pokemons=pokemons)
